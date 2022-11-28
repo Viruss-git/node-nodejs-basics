be459bcedae136288c9e5d8ae58ebdd739b7ad73
Viruss-git <sergei-6@bk.ru>
2022-11-28 23:16:25 +0200

    feat: create and read task - done

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import fs from 'fs';
+import path from 'path';
+
+// Костыль для решения ошибки
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log(__dirname);
+
 const create = async () => {
-    // Write your code here 
+    fs.writeFile(
+        path.join(__dirname, 'files', 'fresh.txt'),
+        "I am fresh and young",
+        (err) => {
+            if (err) {
+                throw 'FS operation failed';
+            }
+        }
+    );
 };
 
 await create();
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,18 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    fs.readFile(
+        path.join(__dirname, 'files', 'fileToRead.txt'),
+        (err, data) => {
+            if (err) {
+                throw 'FS operation failed';
+            }
+            console.log(Buffer.from(data).toString());
+        });
 };
 
 await read();
